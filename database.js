5a04b3e11118f2756be5acb42df450f3a44bc2a4
fn <64966099+f2reninj5@users.noreply.github.com>
2023-11-13 11:42:21 +0000

    Started sequelize database code

---
+++ database.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    dialect: 'mysql',
+    logging: false
+});
+
+const User = sequelize.define('user', {
+    username: {
+        type: DataTypes.STRING(16),
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(32).BINARY,
+        allowNull: false
+    }
+});
+
+module.exports = {
+    sequelize,
+    User
+};
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Express = require('express');
+const { createHash } = require('crypto');
+const { sequelize, User } = require('./database');
+require('dotenv').config();
 
 const app = new Express();
 
 app.use(Express.static('public'));
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080...');
-});
+// let p = createHash('sha256').update('a' + process.env.SALT).digest('binary');
+
+sequelize.sync({ force: false })
+    .then(app.listen(process.env.PORT))
+    .then(() => {
+        console.log(`Listening on port ${process.env.PORT}...`);
+    });
