63f49c54e6957049762f2612a5a0762f68f18b6f
fn <64966099+f2reninj5@users.noreply.github.com>
2024-01-18 13:15:21 +0000

    Updated start message

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const app = require('./app');
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening at port http://localhost:${process.env.PORT}...`);
+    console.log(`Listening at http://localhost:${process.env.PORT}...`);
 });
